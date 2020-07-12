36e17d2409613b41648ae85a494585666f10a2ee
Lilian Maurin <lilianmaurin.wcs2019@gmail.com>
2020-07-12 18:07:24 +0200

    Add Psysh Bundle, pluralize Twig function

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\PinRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -10,8 +11,10 @@ class PinsController extends AbstractController
     /**
      * @Route("/", name="app_home")
      */
-    public function index()
+    public function index(PinRepository $pinRepository)
     {
-        return $this->render('pins/index.html.twig');
+        $pins = $pinRepository->findAll();
+
+        return $this->render('pins/index.html.twig', compact('pins'));
     }
 }
