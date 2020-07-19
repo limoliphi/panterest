cd0cf1d60d0a9913112c1a0f74506a05ed7e53c4
Lilian Maurin <lilianmaurin.wcs2019@gmail.com>
2020-07-19 10:52:36 +0200

    Add flash messages and move base layout to layouts folder

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -40,6 +40,8 @@ class PinsController extends AbstractController
             $em->persist($pin);
             $em->flush();
 
+            $this->addFlash('success', 'Pin successfully created !');
+
             return $this->redirectToRoute('app_home');
         }
 
@@ -70,6 +72,9 @@ class PinsController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $em->flush();
 
+            $this->addFlash('success', 'Pin successfully updated !');
+
+
             return $this->redirectToRoute('app_home');
         }
 
@@ -87,6 +92,9 @@ class PinsController extends AbstractController
         if ($this->isCsrfTokenValid('pin_deletion_' . $pin->getId(), $request->request->get('csrf_token'))) {
             $em->remove($pin);
             $em->flush();
+
+            $this->addFlash('info', 'Pin successfully deleted !');
+
         }
 
         return $this->redirectToRoute('app_home');
